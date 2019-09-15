8062898cedf692ef4eab8cdf2b36b4f8db817736
j0i1b <j0i1b@ugrad.cs.ubc.ca>
2019-09-14 17:52:08 -0700

    finished configureing sendgrip, but cannot manually send

--- server/services/Mailer.js
+++ server/services/Mailer.js
@@ -1,22 +1,55 @@
-const sgMail = require("@sendgrid/mail");
-const keys = require("../config/keys");
- 
-class Mailer {
+const sendgrid = require('sendgrid');
+const helper = sendgrid.mail;
+const keys = require('../config/keys');
+
+class Mailer extends helper.Mail {
   constructor({ subject, recipients }, content) {
-    sgMail.setApiKey(keys.sendGridKey);
-    this.msg = {
-      to: recipients.map(({ email }) => email),
-      from: "no-reply@emaily.com",
-      subject: subject,
-      html: content,
-      trackingSettings: { enable_text: true, enabled: true }
-    };
+    super();
+
+    this.sgApi = sendgrid(keys.sendGridKey);
+    this.from_email = new helper.Email('no-reply@emaily.com');
+    this.subject = subject;
+    this.body = new helper.Content('text/html', content);
+    this.recipients = this.formatAddresses(recipients);
+
+    this.addContent(this.body);
+    this.addClickTracking();
+    this.addRecipients();
   }
- 
+
+  formatAddresses(recipients) {
+    return recipients.map(({ email }) => {
+      return new helper.Email(email);
+    });
+  }
+
+  addClickTracking() {
+    const trackingSettings = new helper.TrackingSettings();
+    const clickTracking = new helper.ClickTracking(true, true);
+
+    trackingSettings.setClickTracking(clickTracking);
+    this.addTrackingSettings(trackingSettings);
+  }
+
+  addRecipients() {
+    const personalize = new helper.Personalization();
+
+    this.recipients.forEach(recipient => {
+      personalize.addTo(recipient);
+    });
+    this.addPersonalization(personalize);
+  }
+
   async send() {
-    const response = await sgMail.send(this.msg);
+    const request = this.sgApi.emptyRequest({
+      method: 'POST',
+      path: '/v3/mail/send',
+      body: this.toJSON()
+    });
+
+    const response = await this.sgApi.API(request);
     return response;
   }
 }
- 
+
 module.exports = Mailer;
