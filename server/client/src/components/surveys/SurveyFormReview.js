bdffb7faf57556376bee13195efcdd621516f08d
j0i1b <j0i1b@ugrad.cs.ubc.ca>
2019-10-06 14:03:16 -0700

    fixed the reroute issue caused by surveyRoutes not returning the user

--- server/client/src/components/surveys/SurveyFormReview.js
+++ server/client/src/components/surveys/SurveyFormReview.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
 import formFields from './formFields';
-import { withRouter} from 'react-router-dom';
+import { withRouter} from 'react-router-dom'
 import * as actions from '../../actions';
 
 const SurveyReview = ({ onCancel, formValues, submitSurvey, history }) => {
